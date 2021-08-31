0c7433736a604a2c3de1b7b3016335bd40c9c7ec
sultan-elayan <sultan.m.elayan@gmail.com>
2021-08-31 19:54:23 +0300

    done lab-18

---
+++ delete-people/index.js
@@ -0,0 +1,26 @@
+
+'use strict';
+
+const peopleSchema = require('./people-schema');
+
+exports.handler = async (event) => {
+  try {
+    const id = event.pathParameters ? event.pathParameters.id : null;
+
+    if (id) {
+      await peopleSchema.delete({ id: id });
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "delete successfully" }),
+    };
+  } catch (error) {
+    return {
+      status: 500,
+      message: error.message,
+    };
+  }
+};
+
+
